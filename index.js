16e745291d243eb1d945d8b20f73053dc9f60250
Shyan Kashani <shyankashani1@gmail.com>
2017-08-07 15:24:55 -0700

    Includes command line options for adding Xs or Os

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 var ticTacToe = require('commander');
 
+ticTacToe.zeroZero = '-';
+ticTacToe.oneZero = '-';
+ticTacToe.twoZero = '-';
+
+ticTacToe.zeroOne = '-';
+ticTacToe.oneOne = '-';
+ticTacToe.twoOne = '-';
+
+ticTacToe.zeroTwo = '-';
+ticTacToe.oneTwo = '-';
+ticTacToe.twoTwo = '-';
+
 ticTacToe
   .version('0.0.1')
-  .option('participate [participant]')
+  .option('zeroZero [zeroZero]')
+  .option('oneZero [oneZero]')
+  .option('twoZero [twoZero]')
+  .option('zeroOne [zeroOne]')
+  .option('oneOne [oneOne]')
+  .option('twoOne [twoOne]')
+  .option('zeroTwo [zeroTwo]')
+  .option('oneTwo [oneTwo]')
+  .option('twoTwo [twoTwo]')
   .parse(process.argv)
 
-console.log(ticTacToe.participate);
-
-var zerozero = '- ';
-var onezero = '- ';
-var twozero = '- ';
-
-var zeroone = '- ';
-var oneone = '- ';
-var twoone = '- ';
-
-var zerotwo = '- ';
-var onetwo = '- ';
-var twotwo = '- ';
+if (ticTacToe.zeroTwo === 'X' && ticTacToe.oneTwo === 'X' && ticTacToe.twoTwo === 'X') {
+  console.log('X wins!');
+}
 
 console.log(
-        '- 0 1 2'
-  + '\n' + '0 ' + zerozero + onezero + twozero
-  + '\n' + '1 ' + zeroone + oneone + twoone
-  + '\n' + '2 ' + zerotwo + onetwo + twotwo
+        '  0 1 2'
+  + '\n' + '0 ' + ticTacToe.zeroZero + ' ' + ticTacToe.oneZero + ' ' + ticTacToe.twoZero
+  + '\n' + '1 ' + ticTacToe.zeroOne + ' ' + ticTacToe.oneOne + ' ' + ticTacToe.twoOne
+  + '\n' + '2 ' + ticTacToe.zeroTwo + ' ' + ticTacToe.oneTwo + ' ' + ticTacToe.twoTwo
 )
